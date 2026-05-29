751218970303a435e00984154ee32897464a1bd4
Geraldine Montellanos <montellanos01@gmail.com>
2026-05-29 02:08:21 -0700

    fixed word reveal

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ const revealObserver = new IntersectionObserver((entries) => {
       revealObserver.unobserve(entry.target);
     }
   });
-}, { threshold: 0.3 });
+}, { threshold: 0.1, rootMargin: '0px 0px -50px 0px' });
 
 document.querySelectorAll('.reveal-text').forEach(el => revealObserver.observe(el));
 
